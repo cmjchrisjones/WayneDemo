5c72deea36503cbcbe84b873b5a5695d2b1494c4
Chris Jones <cmj.chris.jones@googlemail.com>
2021-12-22 16:29:13 +0000

    Demo for Wayne

---
+++ Models/AADUsersViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class AADUsersViewModel
+
+    {
+        public List<User> Users { get; set; }
+    }
+
+    public static class APISimulator
+    {
+        public async static Task<List<User>> SimulateAPIUsersCall()
+        {
+            return new List<User>
+        {
+            new User { Name = "Chris", Email = "chris@ggg.com", Username = "chrisjjjj"},
+            new User { Name = "Wayne", Email = "wayne@jjj.com", Username = "wayne"},
+            new User { Name = "Greg", Email = "greg@ggg.com", Username = "gregg"},
+        };
+        }
+    }
+}
